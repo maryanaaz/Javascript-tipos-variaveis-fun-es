7b4acf76ce6a181068ed8815dffbe7fb1cb067d0
maryana <mayana.azarias@escola.pr.gov.br>
2024-06-20 12:31:18 -0300

    adicionado arquivo da aula

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// Tipo Dado
+// booleanos
+
+// Conversao Implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);
+
+// Conversao implicita
+// Acontece quando o javascript converte o numero em string
+// e depois compara
+// as 2 strings
+console.log(numero === numeroString);
+console.log(numero + numeroString);
+
+// Conversao Explicita
+// Number
